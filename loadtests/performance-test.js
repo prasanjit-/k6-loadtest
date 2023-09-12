6d6fe60e4c5a3e4d82b7278aa8b72a98064f4d1c
Prasanjit Singh <mailprasanjit@gmail.com>
2023-09-12 12:54:46 +0530

    Update performance-test.js

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -5,7 +5,7 @@ export let options = {
   duration: "1m",
   vus: 50,
   thresholds: {
-    http_req_duration: ["p(95)<500"] // condition- 95 percent of response times must be below 500ms
+    http_req_duration: ["p(95)<500"] // condition - 95 percent of response times must be below 500ms
   }
 };
 
